1b29859b816461d7a7991928bbdc4ebc0473d806
Piotr Nowinski <pitred007git@gmail.com>
2019-02-23 22:05:48 +0100

    Finish project

--- js/script.js
+++ js/script.js
@@ -1,15 +1,47 @@
-'use strict';
-var temp;
-var output = document.getElementById('tempConvert-output__div');
-output.innerHTML = '<br> Click the button to convert temperature' + '<br><br>' + output.innerHTML;
-var button = document.getElementById('tempConvert__button');
+var CtoFBtn = document.getElementById('CtoF__button');
+var FtoCBtn = document.getElementById('FtoC__button')
+var output = document.getElementById('output__div');
+var output2 = document.getElementById('output__info');
+var state;
+output.innerHTML = '<br> Click the button to convert temperature' + '<br><br>';
 
+CtoF__button.addEventListener('click', function () {
+    var tempC = window.prompt("Enter temperature in C");
+    if (tempC === "" || tempC === isNaN || tempC === null) {
+        output.innerHTML = ('<br> Incorrect value');
+        output2.innerHTML = ('');
+    } else if (tempC < 0) {
+        state = 'In this temperature water is changing to ICE';
+    } else if (tempC >= 0 && tempC < 100) {
+        state = 'In this temperature water is LIQUID';
+    } else {
+        state = 'In this temperature water is changing to STEAM';
+    }
 
+    tempC = parseInt(tempC);
+    if (!isNaN(tempC)) {
+        var tempF = Math.round(tempC * 9 / 5 + 32);
+        output.innerHTML = '<br> It is ' + tempF + ' &#176;' + 'F <br><br>';
+        output2.innerHTML = '<br>' + state + '<br>'
+    }
+});
 
-button.addEventListener('click', function () {
-
-    userName = window.prompt('What is your name?');
-
-
-    output.innerHTML = 'Hello ' + userName + '!' + '<br><br>' + output.innerHTML;
+FtoC__button.addEventListener('click', function () {
+    var tempF = window.prompt('Enter temperature in F');
+    if (tempF === "" || tempF === isNaN || tempF === null) {
+        output.innerHTML = ('<br> Incorrect value');
+        output2.innerHTML = ('');
+    } else if (tempF < 32) {
+        state = 'In this temperature water is changing to ICE';
+    } else if (tempF >= 32 && tempF < 212) {
+        state = 'In this temperature water is LIQUID';
+    } else {
+        state = 'In this temperature water is changing to STEAM';
+    }
+    tempF = parseInt(tempF);
+    if (!isNaN(tempF)) {
+        var tempC = Math.round((tempF - 32) * 5 / 9);
+        output.innerHTML = '<br> It is ' + tempC + ' &#176;' + 'C <br><br>';
+        output2.innerHTML = '<br>' + state + '<br>'
+    }
 });
